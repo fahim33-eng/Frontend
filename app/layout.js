5a7c01d1016aefe99965ec376b73951320a9c2a0
Fahim <littlefahim2000@gmail.com>
2023-08-04 01:19:59 +0600

    Bug Fixed

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='overflow-hidden'>
+    <html lang="en" className=''>
       <body className={montserrat.className}>
-        
         {children}
         </body>
     </html>
--- components/DoctorsList.jsx
+++ components/DoctorsList.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-
-async function getData() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    if (!res.ok) {
-      throw new Error('Failed to fetch data')
-    }
-    return res.json()
-  }
-
-export default async function DoctorsList() {
-  const data = await getData()
-  if (data) console.log(data)
+"use client"
+import React, { useEffect, useState } from 'react'
+import DoctorCard from './DoctorCard'
+export default function DoctorsList() {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+ 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        console.log(data)
+        setLoading(false)
+      })
+  }, [])
   return (
-    <div>
-      
+    <div className='grid grid-cols-4 gap-16 my-10'>
+      {data?.map(doctor => <DoctorCard id={doctor.id} rating={5.0} hospital={doctor.address.city} fee={800} name={doctor.name} imageUrl={"/surgeon.jpg"} />)}
     </div>
   )
 }
