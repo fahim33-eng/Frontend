5c9f4ebdbd8ba02bbe514ad86627fd68c5b71ae3
Fahim <littlefahim2000@gmail.com>
2023-08-15 19:54:23 +0600

    AgoraRTC Update

--- app/video/[id]/page.js
+++ app/video/[id]/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='bg-gradient-to-r from-[#40a1ce] to-[#bfecfa]'>
+    <div className='bg-gradient-to-r from-[#40a1ce] to-[#bfecfa] min-h-screen w-full'>
       <VideoRoom />
     </div>
   )
