11ffd11b4e0e90271930f7ac4c4700a74c51bb1f
Fahim <littlefahim2000@gmail.com>
2023-08-18 18:20:49 +0600

    Agora Video Problem

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -1,9 +1,11 @@
+"use client"
 import React, { useEffect, useRef } from 'react'
 
 export default function VideoPlayer({ user }) {
   const userRef = useRef()
     useEffect(() => {
         user?.videoTrack.play(userRef.current)
+        user?.audioTrack.play(userRef.current)
     }, [])
   return (
     <div ref={userRef} className='h-full w-full rounded-md'>
